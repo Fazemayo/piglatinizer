e3e2bc94c3a17507c57e38308340a2a6a3585fd0
Fazemayo <connerr3464@fda2.net>
2019-10-29 22:22:35 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-function isVowel(c) {
-    return ['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) !== -1;
-}
-
-$( "document" ).ready(function() {
-    $("#button").click(function() {
-        var name = $(".input").val();
-        var fist= name.charAt(0);
-        var bool = isVowel(fist);
-        $(".output").text(bool);
 
 
-    });
-});
+        <!DOCTYPE html>
+<html>
+    <head>
+        <title> Pig Latinizer </title>
+
+        <!-- Latest compiled and minified CSS -->
+        <!-- Your CSS -->
+        <link type="text/css" rel="stylesheet" href="css/style.css" >
+        <!-- jQuery -->
+        <!-- Your jQuery and Javascript -->
+        <script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+        <script src="js/script.js"></script>
+    </head>
+
+    <body>
+       <div class="container">
+            <h1 class="heading">Piglatinizer</h1>
+            <input class="input" type="text" placeholder="Type some words here" >
+            <button id="button" type="submit">Go!</button>
+
+            <div class="output"></div>
+            </div>
+ 	</body>
+
+</html>
+
+
+
